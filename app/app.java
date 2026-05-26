1210dad85383702e6146002385d084c6ba2d3e3c
MrxForte <urazimbetovazamat6@gmail.com>
2026-05-26 14:34:55 -0700

    first commit

---
+++ app/app.java
@@ -0,0 +1,11 @@
+package app;
+
+class App {
+    public static void main(String[] args) {
+        var stars = "*******************************";
+        System.out.println(stars);
+        System.out.println("Fee App in Java");
+        System.out.println(stars);
+
+    }
+}
